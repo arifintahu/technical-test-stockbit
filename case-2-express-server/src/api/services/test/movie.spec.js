a57eb4c3fb2fe9798480604350524e3ed460a6d8
Miftahul Arifin <miftahul97@gmail.com>
2021-11-03 07:35:02 +0700

    [CASE 2] Add test for movie service

---
+++ case-2-express-server/src/api/services/test/movie.spec.js
@@ -0,0 +1,10 @@
+const { expect } = require("chai");
+
+describe("Check movie service", () => {
+  it("Search movie returns movies", () => {
+    expect(true).to.be.true;
+  });
+  it("Detail movie returns a movie detail", () => {
+    expect(true).to.be.true;
+  });
+});
